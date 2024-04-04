0fdc47278a51d4b87caaf81c233c779c11cdb5f1
Nathan Spencer <natekspencer@gmail.com>
2024-04-04 11:26:45 -0600

    Fix ignore comment

--- pyweatherflowudp/device.py
+++ pyweatherflowudp/device.py
@@ -485,8 +485,8 @@ class TempestDevice(AirSensorType, SkySensorType):
         https://help.weatherflow.com/hc/en-us/articles/360048877194-Solar-Power-Rechargeable-Battery
         """
         return WIND_SAMPLE_INTERVAL_POWER_SAVE_MODE_MAP.get(
-            self._wind_sample_interval,
-            PowerSaveMode.UNKNOWN,  # type: ignore
+            self._wind_sample_interval, # type: ignore
+            PowerSaveMode.UNKNOWN,
         )
 
     # Derived metrics
